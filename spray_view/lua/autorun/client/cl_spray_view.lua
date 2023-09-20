5a6f1d5135fd0405747bdb4f1e25b6f46a15813d
le-me <133717528+le-me@users.noreply.github.com>
2023-09-20 18:21:09 -0400

    e

--- spray_view/lua/autorun/client/cl_spray_view.lua
+++ spray_view/lua/autorun/client/cl_spray_view.lua
@@ -1,43 +1,89 @@
 local sprays = {}
+local mins = Vector(0, -32, -32)
+local maxs = Vector(2, 32, 32)
+local color_red = Color(255, 0, 0, 255)
+
+local function RemoveSpray(steamID)
+    for i = #sprays, 1, -1 do
+        if steamID == nil or sprays[i].steamID == steamID then
+            table.Empty(sprays[i])
+            table.remove(sprays, i)
+        end
+    end
+end
 
 net.Receive("spray_view", function()
     if not LocalPlayer():IsAdmin() then return end -- It doesn't matter if this is bypassed, who cares if other people see sprays. Just to avoid inconvenience for other players
 
     local steamID = net.ReadString()
+    local name = net.ReadString()
     local hitPos = net.ReadVector()
+    local normal = net.ReadVector()
 
-    if steamID:len() < 1 or hitPos:IsZero() then
-        for i = #sprays, 1, -1 do
-            if sprays[i].steamID == steamID then
-                sprays[i].steamID = nil -- Empty table
-                sprays[i].hitPos = nil
+    RemoveSpray(steamID)
 
-                table.remove(sprays, i)
-            end
-        end
-
+    if steamID:len() < 1 or name:len() < 1 or hitPos:IsZero() or normal:IsZero() then -- Player left or something went wrong
         return
+    end
+
+    local angle = normal:Angle()
+    if angle.pitch > 0 or angle.pitch < -0 then
+        angle:SnapTo("y", 90)
     end
 
     table.insert(sprays, {
         steamID = steamID,
-        hitPos = hitPos
+        name = name,
+        hitPos = hitPos,
+        angle = angle
     })
 end)
 
-hook.Add("PostDrawHUD", "spray_view", function()
-    local hitPos = LocalPlayer():GetEyeTrace().HitPos
+local function DrawCentered(x, y, text)
+    local tw, th = surface.GetTextSize(text)
+    surface.SetTextPos(x - (tw / 2), y)
+    surface.DrawText(text)
 
-    for i = 1, #sprays do
+    return y + th
+end
+
+hook.Add("PostDrawHUD", "spray_view", function()
+    local localPlayer = LocalPlayer()
+    local hitPos = localPlayer:GetEyeTrace().HitPos
+
+    for i = #sprays, 1, -1 do
         if not sprays[i].hitPos then continue end -- Wtflip
 
         if hitPos:IsEqualTol(sprays[i].hitPos, 32) then
-            surface.SetFont("BudgetLabel")
-            surface.SetTextColor(255, 255, 255, 255)
+            if localPlayer:KeyDown(IN_ATTACK2) and not localPlayer:KeyDownLast(IN_ATTACK2) then
+                table.remove(sprays, i)
+                break
+            end
 
-            local tw, th = surface.GetTextSize(sprays[i].steamID)
-            surface.SetTextPos((ScrW() / 2) - (tw / 2), (ScrH() / 2) - 256)
-            surface.DrawText(sprays[i].steamID)
+            cam.Start2D()
+                surface.SetFont("BudgetLabel")
+                surface.SetTextColor(255, 255, 255, 255)
+
+                local x = ScrW() / 2
+                local y = (ScrH() / 2) - 256
+
+                y = DrawCentered(x, y, sprays[i].steamID)
+                y = DrawCentered(x, y, sprays[i].name)
+                y = DrawCentered(x, y, "")
+
+                local attack2 = input.LookupBinding("+attack2", true)
+
+                if attack2 then
+                    surface.SetTextColor(255, 0, 0, 255)
+                    DrawCentered(x, y, string.format("Press %s to remove from local database", language.GetPhrase(attack2)))
+                end
+            cam.End2D()
+
+            local view = render.GetViewSetup() -- Fix cam.Start3D sucking
+
+            cam.Start3D(view.origin, view.angles, view.fov, view.x, view.y, view.width, view.height, view.znear, view.zfar)
+                render.DrawWireframeBox(sprays[i].hitPos, sprays[i].angle, mins, maxs, color_red, true)
+            cam.End3D()
 
             break
         end
--- spray_view/lua/autorun/server/sv_spray_view.lua
+++ spray_view/lua/autorun/server/sv_spray_view.lua
@@ -1,18 +1,32 @@
 -- Potential issue with players joining and sprays not existing for them until re-sprayed, need to investigate
 
+local vector_up = Vector(0, 0, 4)
+
+local traceResult = {}
+
+local traceData = {
+    mask = MASK_SOLID_BRUSHONLY,
+    collisiongroup = COLLISION_GROUP_NONE,
+    output = traceResult
+}
+
+
 util.AddNetworkString("spray_view")
 
 hook.Add("PlayerSpray", "spray_view", function(pPlayer)
-    local tr = pPlayer:GetEyeTraceNoCursor()
+    traceData.start = pPlayer:EyePos()
+    traceData.start:Add(vector_up)
+
+    traceData.endpos = pPlayer:GetForward()
+    traceData.endpos:Mul(128)
+    traceData.endpos:Add(traceData.start)
+
+    util.TraceLine(traceData)
 
     net.Start("spray_view")
         net.WriteString(pPlayer:SteamID())
-        net.WriteVector(tr.HitPos)
-    net.Broadcast()
-end)
-
-hook.Add("PlayerDisconnected", "spray_view", function(pPlayer)
-    net.Start("spray_view")
-        net.WriteString(pPlayer:SteamID())
+        net.WriteString(pPlayer:GetName())
+        net.WriteVector(traceResult.HitPos)
+        net.WriteVector(traceResult.HitNormal)
     net.Broadcast()
 end)
